6886f1481034d505ce44166d98a3657b92d3e2d5
SpicyChickenFLY <xixiayuchao@gmail.com>
2021-04-03 09:55:21 +0800

    complete stack

--- utils/list.go
+++ utils/list.go
@@ -1,1 +1,25 @@
 package utils
+
+type List interface {
+	GetHead()
+	GetTail()
+	PushFront()
+	PopFront()
+	PushBack()
+	PopBack()
+}
+
+type MyListNode struct {
+	Value interface{}
+	Next  *ListNode
+}
+
+func NewMyList(initVals []interface{}) *ListNode {
+	newList = nil
+	for i := len(initVals) - 1, i >= 0; i-- {
+		newList := &ListNode{Value: initVals[i], Next: ms.top}
+	}
+	return newList
+}
+
+func (l *ListNode) Print() {}
--- utils/stack.go
+++ utils/stack.go
@@ -1,1 +1,50 @@
 package utils
+
+import "errors"
+
+type Stack interface {
+	IsEmpty() bool
+	Push(value interface{})
+	Top() (interface{}, error)
+	Pop() (interface{}, error)
+}
+
+type MyStack struct {
+	top  *ListNode
+	size int
+}
+
+/** Initialize your data structure here. */
+func NewMyStack(initVals []interface{}) *MyStack {
+	newStack := &MyStack{
+		top: nil,
+	}
+	for _, initVal := range InitVals {
+		newStack.Push(initVal)
+	}
+	return newStack
+}
+
+func (ms *MyStack) IsEmpty() bool {
+	return ms.top == nil
+}
+
+func (ms *MyStack) Push(value interface{}) {
+	ms.top = &ListNode{Value: value, Next: ms.top}
+}
+
+func (ms *MyStack) Top() (interface{}, error) {
+	if ms.top == nil {
+		return nil, errors.New("Empty Stack")
+	}
+	return ms.top.Value, nil
+}
+
+func (ms *MyStack) Pop() (interface{}, error) {
+	if ms.top == nil {
+		return nil, errors.New("Empty Stack")
+	}
+	value := ms.top.Value
+	ms.top = ms.top.Next
+	return value, nil
+}
