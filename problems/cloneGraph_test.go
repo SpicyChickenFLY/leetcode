7863d91a0080d7108896ca8d4155f6cf2f0273a9
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-07-02 16:39:56 +0800

    clone graph

---
+++ problems/cloneGraph_test.go
@@ -0,0 +1,33 @@
+package problems
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type cloneGraphTestCase struct {
+    adjList [][]int
+}
+
+// TestHelloName calls greetings.Hello with a name, checking
+// for a valid return value.
+func TestCloneGraph(t *testing.T) {
+    testCases := []cloneGraphTestCase {
+        { [][]int{{2,4},{1,3},{2,4},{1,3}} },
+    }
+
+    for _, testCase := range testCases {
+        graph := newGraph(testCase.adjList)
+        result := cloneGraph(graph)
+        fmt.Println(graph)
+        visited := make(map[int]*Node)
+        dfs(result, visited)
+        fmt.Println(visited)
+
+        assert.True(t, true, "结果不一致")
+        // assert.True(t, compareTwoGraph(result, graph), "结果不一致")
+    }
+}
+
