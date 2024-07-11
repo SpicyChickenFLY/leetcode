2e5afc35316b47f48d82da57d16ca698222aedce
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-07-11 13:49:09 +0800

    furthest building you can reach

---
+++ problems/furthestBuildingYouCanReach.go
@@ -0,0 +1,48 @@
+package problems
+
+import "container/heap"
+
+type IntHeap []int
+
+func (h IntHeap) Less(i, j int) bool  { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h IntHeap) Len() int            { return len(h) }
+func (h *IntHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	x := old[len(old)-1]
+	*h = old[:len(old)-1]
+	return x
+}
+
+func furthestBuilding(heights []int, bricks int, ladders int) int {
+	ladderUsers := &IntHeap{}
+	heap.Init(ladderUsers)
+
+	for i := 0; i < len(heights)-1; i++ {
+		diff := heights[i+1] - heights[i]
+
+		// 下楼免费, 否则上楼重新计算成本
+		if diff < 0 {
+			continue
+		}
+
+		// 梯子有空余肯定先用梯子
+		heap.Push(ladderUsers, diff)
+
+        // 梯子不够用把不配用梯子的踢出来
+		if ladderUsers.Len() > ladders {
+			replaced := heap.Pop(ladderUsers).(int)
+
+			// 剩余砖块不足以跨越这栋楼,就结束了
+			if bricks < replaced {
+				return i
+			}
+
+			// 被踢出的就用砖块替代
+			bricks -= replaced
+		}
+
+	}
+	return len(heights) - 1
+}
