9064025aaab34d677bb11246d950e95e93db6973
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-08-16 17:01:19 +0800

    substract deps

--- problems/furthestBuildingYouCanReach.go
+++ problems/furthestBuildingYouCanReach.go
@@ -1,22 +1,10 @@
 package problems
 
 import "container/heap"
-
-type IntHeap []int
-
-func (h IntHeap) Less(i, j int) bool  { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
-func (h IntHeap) Len() int            { return len(h) }
-func (h *IntHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
-func (h *IntHeap) Pop() interface{} {
-	old := *h
-	x := old[len(old)-1]
-	*h = old[:len(old)-1]
-	return x
-}
+import "github.com/SpicyChickenFLY/go-leetcode/utils"
 
 func furthestBuilding(heights []int, bricks int, ladders int) int {
-	ladderUsers := &IntHeap{}
+	ladderUsers := &utils.IntHeap{}
 	heap.Init(ladderUsers)
 
 	for i := 0; i < len(heights)-1; i++ {
